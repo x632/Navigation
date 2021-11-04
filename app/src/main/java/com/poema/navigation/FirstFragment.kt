f24c7dd34ad4ba5ee550e9e8b6c320373a8de2d5
Andreas Svedstedt <andreas@poema.se>
2021-11-04 16:51:24 +0100

    Initial commit

---
+++ app/src/main/java/com/poema/navigation/FirstFragment.kt
@@ -0,0 +1,31 @@
+package com.poema.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.poema.navigation.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(2,"An")
+        binding.tv1.setOnClickListener { Navigation.findNavController(view).navigate(action) }
+
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/poema/navigation/SecondFragment.kt
@@ -0,0 +1,33 @@
+package com.poema.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import com.poema.navigation.databinding.FragmentFirstBinding
+import com.poema.navigation.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+    val args: SecondFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.tv2.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment) }
+        val myNumber = "${args.number} ${args.testString}"
+        binding.tv2.text = myNumber
+        return view
+    }
+
+}
